d85be5ba85f7ab7bdd272755448dcffde0b42369
alffa <fabricio.alffa@gmail.com>
2020-01-22 14:32:46 -0700

    calse 12 de python, añadimos un menu y mas funcionalidad al CRUD, Creamos cliente y vemos si existe o no dicho cliente

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ clients='pablo,ricardo,'
 def create_client(client_name):
     global clients
 
-    clients += client_name
-    _add_comma()
+    if client_name not in clients:
+        clients += client_name
+        _add_comma()
+    else:
+        print('Client already is in the client\'s list')
 
 def list_clients():
     global clients
@@ -18,7 +21,24 @@ def _add_comma():
     clients += ','
 
 
+def _print_welcome():
+    print('Welcome To Platzi Ventas')
+    print('*'*50)
+    print('What would you like to do today?')
+    print('(C)reate client')
+    print('(D)elete client')
+
+
 if __name__=='__main__':
-    list_clients()
-    create_client('David')
-    list_clients()
+    _print_welcome()
+    
+    command =input()
+
+    if command=='C':
+        client_name = input('What is the client name?')
+        create_client(client_name)
+        list_clients()
+    elif command=='D':
+        pass
+    else:
+        print('Invalid command')
