6248459791c95f70f0a3c9ae96214829c6d00b8e
alffa <fabricio.alffa@gmail.com>
2020-01-22 11:34:55 -0700

    un cuadrado con turtle

--- turtle_square.py
+++ turtle_square.py
@@ -3,5 +3,13 @@ import turtle
 windows = turtle.Screen()
 tortuga = turtle.Turtle()
 tortuga.forward(100)
+tortuga.right(90)
+tortuga.forward(100)
+tortuga.right(90)
+tortuga.forward(100)
+tortuga.right(90)
+tortuga.forward(100)
+tortuga.right(90)
+
 
 windows.mainloop()
