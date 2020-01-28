f9420872033033c18036c5570d19b933e438aad1
alffa <fabricio.alffa@gmail.com>
2020-01-28 11:14:17 -0700

    clase 15 creamos la opcion update para la cual se creo una funcion _get_client_name() usada para pedir el nombre del cliente en update y en create client ademas se uso upper para cuando se solicita el comando que vamos a usas (C R U D) por si el usuario teclea el comando en minusculas automaticamente se convierte en mayuscula y hacemos match en la opcion, por ultimo se creo la funcion update client que recibe dos parametros, el usuario que vamos a actualizar y la actualizacion y lo reemplazamos con clients.replace()

--- main.py
+++ main.py
@@ -15,6 +15,15 @@ def list_clients():
     print(clients)
 
 
+def updated_client(client_name,updated_client_name):
+    global clients
+
+    if client_name in clients:
+        clients=clients.replace(client_name+',',updated_client_name+',')
+    else:
+        print('Client in not in clients list')
+
+
 def _add_comma():
     global clients
 
@@ -26,19 +35,29 @@ def _print_welcome():
     print('*'*50)
     print('What would you like to do today?')
     print('(C)reate client')
+    print('(U)pdate client')
     print('(D)elete client')
+
+
+def _get_client_name():
+    return input('What is the Client name?')
 
 
 if __name__=='__main__':
     _print_welcome()
     
     command =input()
-
+    command = command.upper();
     if command=='C':
-        client_name = input('What is the client name?')
+        client_name=_get_client_name()
         create_client(client_name)
         list_clients()
     elif command=='D':
         pass
+    elif command =='U':
+        client_name=_get_client_name()
+        updated_client_name=input('What is the updated client name ?')
+        updated_client(client_name,updated_client_name)
+        list_clients()
     else:
         print('Invalid command')
