8675682b6d2aa9fbba52baaf7473fa38da0b0c83
alffa <fabricio.alffa@gmail.com>
2020-01-28 13:17:31 -0700

    clase 18 se implementa la opcion (S)earch para la cual se creo una funcion search_client que recibe como parametro client_name y utiliza el metodo split(',') que explota la lista clients en todas las partes separadas por coma (',') y buscamos al cliente con un for en caso de encontrarlo regresamos TRUE, al realizar las iteraciones con el for y hacer la comparacion con el parametro recibido y el valor actual de client en el for en caso de no ser iguales utilizamos un continue para seguir con las iteraciones

--- main.py
+++ main.py
@@ -37,6 +37,17 @@ def delete_client(client_name):
     else:
         client_not_list()
 
+    
+def search_client(client_name):
+    clients_list=clients.split(',')
+
+    for client in clients_list:
+        if client != client_name:
+            continue
+        else:
+            return True
+
+
 def _add_comma():
     global clients
 
@@ -48,8 +59,10 @@ def _print_welcome():
     print('*'*50)
     print('What would you like to do today?')
     print('(C)reate client')
+    print('(L)ist')
     print('(U)pdate client')
     print('(D)elete client')
+    print('(S)earch client')
 
 
 def _get_client_name():
@@ -65,10 +78,20 @@ if __name__=='__main__':
         client_name=_get_client_name()
         create_client(client_name)
         list_clients()
+    elif command=='L':
+        list_clients()
     elif command=='D':
         client_name=_get_client_name()
         delete_client(client_name)
         list_clients()
+    elif command=='S':
+        client_name=_get_client_name()
+        found=search_client(client_name)
+    
+        if found:
+            print('The client is in the client\'s list')
+        else:
+            print('The client: {} is not in our client\'s list'.format(client_name))
     elif command =='U':
         client_name=_get_client_name()
         updated_client_name=input('What is the updated client name ?')
